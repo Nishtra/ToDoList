6a0ad1236e350bbb72d0db37dd33794107e3b242
student <id.yuuko@gmail.com>
2020-11-23 19:53:53 +0200

    Experimental changes to default Angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import {ToDoListItem} from "../app/to-do-list-item";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // template: '<div>Hello world</div>',
+  styleUrls: ['./app.component.css'],
+  styles: ['p {color: red;}']
 })
 export class AppComponent {
   title = 'ToDoListApp';
+
+  getTitle(): string {
+    return this.title;
+  }
 }
